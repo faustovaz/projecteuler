9c1f3d5a2467dc4228ed852dae6c79f03619db37
faustovaz <faustovaz@gmail.com>
2016-02-02 18:05:33 -0200

    A different version using ruby lazy feature

--- 003.rb
+++ 003.rb
@@ -10,36 +10,33 @@
 
 require File.expand_path 'lib/generators'
 include Generators
-require 'set'
+include Math
 
 class Fixnum
   def factors
-    factors = Set.new
+    factors = []
     primes = prime_numbers_generator
     number = self
     while number > 1 do
       if (number.modulo primes.peek).zero?
-        number = number / primes.peek
+        number /= primes.peek
         factors << primes.peek
       else
         primes.next
       end
     end
-    factors.to_a
+    factors
   end
 end
 
 puts 600_851_475_143.factors.last
 
-#More functional way (need to improve)
-prime_numbers_generator.take_while{ |p|
-  p < sqrt(600_851_475_143)
-}.select { |f|
-  (600_851_475_143.modulo f).zero?
-}.last
-
-
-
-
-
-#6857
+#Different version usgin ruby lazy feature
+x = 600_851_475_143
+puts prime_numbers_generator.lazy.take_while{ x > 1 }.select{ |n|
+  a = x
+  while x.modulo(n).zero? do
+    x = x / n
+  end
+  a.modulo(n).zero?
+}.to_a.last
