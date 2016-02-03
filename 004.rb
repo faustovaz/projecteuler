0df1e72f9b193972165f49d5aced82ba006294b9
faustovaz <faustovaz@gmail.com>
2016-02-03 16:37:18 -0200

    Solving problem 4

--- 004.rb
+++ 004.rb
@@ -1,5 +1,5 @@
 # *****************************************************************************
-# * Project Euler - Problem 3
+# * Project Euler - Problem 4
 #
 # A palindromic number reads the same both ways. The largest palindrome made
 # from the product of two 2-digit numbers is 9009 = 91 × 99.
@@ -7,3 +7,12 @@
 # Find the largest palindrome made from the product of two 3-digit numbers.
 #
 # ****************************************************************************/
+
+palindromes = []
+999.downto(100).each { |x|
+    found = 999.downto(100).lazy.find{ |j|
+          (x * j).to_s.eql? (x * j).to_s.reverse
+    }
+    palindromes << (x * found) if found
+}
+puts palindromes.sort.last
