cbf9eb2076897a97a317990fbaddd0f4f0b1007c
faustovaz <faustovaz@gmail.com>
2016-02-16 16:57:12 -0200

    ...

--- test/utils_test.rb
+++ test/utils_test.rb
@@ -4,7 +4,7 @@ require File.expand_path '../lib/utils.rb'
 include Utils
 
 describe "Utils" do
-  it "should be a pythagorean triplet" do
+  it "should validate a pythagorean triplet" do
     triplet?([3,4,5]).must_equal true
   end
 end
