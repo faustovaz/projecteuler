7e688e45cdefe9268163acdbd0e3fbd5a92e7a83
faustovaz <faustovaz@gmail.com>
2016-02-17 10:42:23 -0200

    Solving problem 10

---
+++ 010.rb
@@ -0,0 +1,13 @@
+# *****************************************************************************
+# * Project Euler - Problem 10
+#
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+# Find the sum of all the primes below two million.
+# ****************************************************************************/
+
+require File.expand_path 'lib/generators'
+include Generators
+
+p prime_numbers_generator.take_while{ |prime|
+  prime < 2_000_000
+}.reduce(:+)
