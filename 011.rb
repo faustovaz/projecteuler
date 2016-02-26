31b1a0d1f65049bd4c9b0923198f5385970129a0
faustovaz <faustovaz@gmail.com>
2016-02-26 16:31:24 -0300

    Creating the matrix

--- 011.rb
+++ 011.rb
@@ -26,7 +26,7 @@ grid = [
   "20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54",
   "01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
 ].map{ |set_of_numbers|
-    set_of_numbers.split(" ").map{ |number|
-      number.to_i
-    }
+  set_of_numbers.split(" ").map{ |number|
+    number.to_i
   }
+}
