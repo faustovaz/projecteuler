95ae6fae65892c9c4e6d4c428ba7e091c8f0e86a
faustovaz <faustovaz@gmail.com>
2016-03-03 16:47:09 -0300

    Method to get right adjacent numbers

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -1,8 +1,26 @@
 class Matrix
   def initialize(arr)
+    @matrix = arr
   end
 
-  def adjacents_numbers_of(row, column, len)
-    []
+  def all_adjacents_numbers_of(row, column, len)
+    adjacents = []
+    adjacents << get_adjacents_from_the_right(row, column, len)
+    adjacents << get_adjacents_from_the_left(row, column, len)
+    adjacents << get_adjacents_from_up(row, column, len)
+    adjacents << get_adjacents_from_down(row, column, len)
+    adjacents << get_adjacents_from_up_right(row, column, len)
+    adjacents << get_adjacents_from_down_right(row, column, len)
+    adjacents << get_adjacents_from_up_left(row, column, len)
+    adjacents << get_adjacents_from_down_left(row, column, len)
+    adjacents
   end
+
+  def get_adjacents_from_the_right(row, column, len)
+    return [] if @matrix[row].nil? or
+                  @matrix[row][column].nil? or
+                  @matrix[row].slice(column, len + 1).size != len + 1
+    @matrix[row].slice(column, len + 1)
+  end
+
 end
--- test/matrix_test.rb
+++ test/matrix_test.rb
@@ -2,10 +2,29 @@ require 'minitest/autorun'
 require File.expand_path '../lib/matrix.rb'
 
 describe 'Matrix' do
+  describe 'adjacents numbers from the rights of a given element' do
+    before do
+      @matrix = Matrix.new [
+        [1,2,3,4,5,6,7,8],
+        [1,2,3,4,5,6,7,8],
+        [1,2,3,4,5,6,7,8]
+      ]
+    end
 
-  it 'should return all adjacent combination of the matrix' do
-    matrix = Matrix.new [[1,2,3], [1,2,3]]
-    matrix.adjacents_numbers_of(0,0,2).must_equal [[1,2], [1,1]]
+    it 'should return all adjacent numbers from the right' do
+      @matrix.get_adjacents_from_the_right(0,0,2).must_equal [1,2,3]
+    end
+
+    it 'shoud return an empty array' do
+      @matrix.get_adjacents_from_the_right(0,1,7).must_be :empty?
+    end
+
+    it 'should return the only one adjacent number' do
+      @matrix.get_adjacents_from_the_right(1,2,0).must_equal [3]
+    end
+
+    it 'shoud return an empty array when given an invalid input' do
+      @matrix.get_adjacents_from_the_right(3,2,3).must_be :empty?
+    end
   end
-
 end
