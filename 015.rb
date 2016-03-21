9e6af0c17605a5f2d6a90c6cec5eb98c1b435b39
faustovaz <faustovaz@gmail.com>
2016-03-21 18:00:52 -0300

    Staring problem 015.rb

--- 015.rb
+++ 015.rb
@@ -1,13 +1,8 @@
-def run_forest_run(row, column, matrix)
+def run_forest_run(row, column, matrix = [[0] * 3] * 3)
   return 0 if matrix[row].nil? || matrix[row][column].nil?
   return 1 if (matrix.size - 1) == row and (matrix[row].size - 1) == column
   return run_forest_run(row + 1, column, matrix) +
           run_forest_run(row, column + 1, matrix)
 end
 
-def teste(number)
-  return 1 if number == 1
-  return number + teste(number - 1)
-end
-
-puts run_forest_run(0, 0, [[0] * 3] * 3)
+puts run_forest_run(0, 0)
