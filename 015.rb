43bf80a4a00a097aa979d412c9a8444dfa84bece
faustovaz <faustovaz@gmail.com>
2016-03-22 17:42:50 -0300

    Trying to get some performance

--- 015.rb
+++ 015.rb
@@ -1,11 +1,18 @@
 # Problem 15
 # Find how many paths to get to the bottom right corner
 
-def run_forest_run(row, column, matrix = [[0] * 3] * 3)
-  return 0 if matrix[row].nil? || matrix[row][column].nil?
-  return 1 if (matrix.size - 1) == row and (matrix[row].size - 1) == column
-  return run_forest_run(row + 1, column, matrix) +
-          run_forest_run(row, column + 1, matrix)
+class Runner
+  def initialize(matrix = [[0] * 3] * 3)
+    @matrix = matrix
+  end
+
+  def run_forest_run(row, column)
+    return 0 if @matrix[row].nil? || @matrix[row][column].nil?
+    return 1 if (@matrix.size - 1) == row and (@matrix[row].size - 1) == column
+    return run_forest_run(row, column + 1) +
+            run_forest_run(row + 1, column)
+  end
 end
 
-puts run_forest_run(0, 0, [[0] * 20] * 20)
+r = Runner.new [[0] * 20] * 20
+p r.run_forest_run(0,0)
