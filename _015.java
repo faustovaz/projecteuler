4b1228fab717282a74a85ac6680d6488c1ece73a
faustovaz <faustovaz@gmail.com>
2016-03-24 17:34:41 -0300

    Adding a iterative version othe the algorithm

--- _015.java
+++ _015.java
@@ -1,5 +1,8 @@
 // Problem 15
 // Find how many paths there are to get to the bottom right corner
+// Recursive version is much faster than iterative version
+
+import java.util.Stack;
 
 public class _015 {
 
@@ -9,6 +12,7 @@ public class _015 {
 		this.matrix = matrix;
 	}
 
+	//Recursive version
 	public long run_forest_run(int row, int column){
 		try{
 			if (this.matrix.length - 1 == row || this.matrix[row].length -1 == column)
@@ -19,6 +23,53 @@ public class _015 {
 		catch(ArrayIndexOutOfBoundsException e){
 			return 0;
 		}
+	}
+
+	//Iterative Version
+	public long run_forest_run(){
+		Stack<String> stack = new Stack<String>();
+		String current = "0,0";
+		boolean done = false;
+		long sum = 0;
+		while(!done){
+			if(isInBound(current)){
+				if(toInt(current)[0] + 1 == this.matrix.length
+						&& toInt(current)[1] + 1 == this.matrix[0].length ){
+					sum++;
+				}
+				stack.push(current);
+				current = toStr(toInt(current)[0], toInt(current)[1] + 1);
+			}
+			else{
+				if(stack.empty()){
+					done = true;
+				}
+				else{
+					current = stack.peek();
+					stack.pop();
+					current = toStr(toInt(current)[0] + 1, toInt(current)[1]);
+				}
+			}
+		}
+		return sum;
+	}
+
+	public boolean isInBound(String coordinate){
+		int coordinates[] = toInt(coordinate);
+		int row = coordinates[0];
+		int column = coordinates[1];
+		return (row >= 0 && row < this.matrix.length) &&
+				(column >= 0 && column < this.matrix[0].length);
+	}
+
+	public String toStr(int row, int column){
+		return String.valueOf(row) + "," + String.valueOf(column);
+	}
+
+	public int[] toInt(String coord){
+		String coordinates[] = coord.split(",");
+		return new int[]{	Integer.valueOf(coordinates[0]),
+												Integer.valueOf(coordinates[1])};
 	}
 
 	public static void main(String[] args) {
