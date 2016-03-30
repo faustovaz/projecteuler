ddbbca4ead9714d3ac783a9434163c3033ee47d7
faustovaz <faustovaz@gmail.com>
2016-03-30 18:00:47 -0300

    Starting 17

---
+++ 017.rb
@@ -0,0 +1,19 @@
+#Problem 17
+
+units = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine"
+}
+
+dozens = {10: "ten", 20: "twenty", 30: "thirty", 50: "fifty"}
+
+hundred = "hundred"
+
+thousand = "thousand"
