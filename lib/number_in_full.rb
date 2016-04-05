12d2e6731f2eaec0aa212160dc0b0639dd331a02
faustovaz <faustovaz@gmail.com>
2016-04-05 18:02:25 -0300

    Writing two digits in full

--- lib/number_in_full.rb
+++ lib/number_in_full.rb
@@ -1,5 +1,23 @@
 class Fixnum
+
+  @@ones ||= {
+        1 => "one", 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five',
+        6 => "six", 7 => 'seven', 8 => "eight", 9 => "nine", 10 => 'ten',
+        11 => "eleven", 12 => "twelve", 13 => "thirteen",
+        14 => "fourteen", 15 => "fifteen", 16 => "sixteen",
+        17 => "seventeen", 18 => "eighteen", 19 => "nineteen",
+      }
+  @@tens ||={
+        20 => 'twenty', 30 => 'thirty', 40 => 'fourty', 50 => 'fifty',
+        60 => 'sixty', 70 => 'seventy', 80 => 'eigthty', 90 => 'ninety'
+  }
+
   def in_full
-    "not yet"
+    all_numbers = @@ones.merge(@@tens)
+    return all_numbers[self] if all_numbers.keys.include? self
+    number = (self / 10) * 10
+    modulo = self.modulo 10
+    return "#{@@tens[number]} #{@@ones[modulo]}"
   end
+
 end
--- test/number_in_full_test.rb
+++ test/number_in_full_test.rb
@@ -4,5 +4,13 @@ require File.expand_path '../lib/number_in_full'
 describe "Number in full" do
   it "should get a single number in full" do
     1.in_full.must_equal "one"
+    10.in_full.must_equal "ten"
   end
+
+  it "should get two digit number in full" do
+    24.in_full.must_equal "twenty four"
+    53.in_full.must_equal "fifty three"
+    99.in_full.must_equal "ninety nine"
+  end
+
 end
