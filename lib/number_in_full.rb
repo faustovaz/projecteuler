5a21446e9809da014a2502281bda12c8f01182b2
faustovaz <faustovaz@gmail.com>
2016-04-06 16:37:26 -0300

    Adding the in_full method to Fixnum class and some tests

--- lib/number_in_full.rb
+++ lib/number_in_full.rb
@@ -1,23 +1,39 @@
 class Fixnum
 
-  @@ones ||= {
-        1 => "one", 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five',
-        6 => "six", 7 => 'seven', 8 => "eight", 9 => "nine", 10 => 'ten',
-        11 => "eleven", 12 => "twelve", 13 => "thirteen",
-        14 => "fourteen", 15 => "fifteen", 16 => "sixteen",
-        17 => "seventeen", 18 => "eighteen", 19 => "nineteen",
-      }
-  @@tens ||={
-        20 => 'twenty', 30 => 'thirty', 40 => 'fourty', 50 => 'fifty',
-        60 => 'sixty', 70 => 'seventy', 80 => 'eigthty', 90 => 'ninety'
+  @@ones = {
+    1 => "one", 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five',
+    6 => "six", 7 => 'seven', 8 => "eight", 9 => "nine", 10 => 'ten',
+    11 => "eleven", 12 => "twelve", 13 => "thirteen",
+    14 => "fourteen", 15 => "fifteen", 16 => "sixteen",
+    17 => "seventeen", 18 => "eighteen", 19 => "nineteen",
+  }
+  @@tens = {
+    20 => 'twenty', 30 => 'thirty', 40 => 'fourty', 50 => 'fifty',
+    60 => 'sixty', 70 => 'seventy', 80 => 'eigthty', 90 => 'ninety'
+  }
+  @@thousand = {
+    100 => 'one hundred', 1000 => 'one thousand'
   }
 
   def in_full
-    all_numbers = @@ones.merge(@@tens)
+    actual_number = self
+    all_numbers = @@ones.merge(@@tens).merge(@@thousand)
     return all_numbers[self] if all_numbers.keys.include? self
+    return self.three_digit_number_in_full if self > 100
+    self.two_digit_number_in_full
+  end
+
+  def three_digit_number_in_full
+    return "" unless self > 100
+    "#{@@ones[self/100]} hundred and #{(self.modulo 100).two_digit_number_in_full}"
+  end
+
+  def two_digit_number_in_full
+    return "" unless self < 100
     number = (self / 10) * 10
     modulo = self.modulo 10
-    return "#{@@tens[number]} #{@@ones[modulo]}"
+    return "#{@@tens[number]} #{@@ones[modulo]}" if number > 0
+    "#{@@ones[modulo]}"
   end
 
 end
--- test/number_in_full_test.rb
+++ test/number_in_full_test.rb
@@ -8,9 +8,18 @@ describe "Number in full" do
   end
 
   it "should get two digit number in full" do
+    18.in_full.must_equal "eighteen"
     24.in_full.must_equal "twenty four"
     53.in_full.must_equal "fifty three"
     99.in_full.must_equal "ninety nine"
+  end
+
+  it "should get three digit number in full" do
+    157.in_full.must_equal "one hundred and fifty seven"
+    623.in_full.must_equal "six hundred and twenty three"
+    832.in_full.must_equal "eight hundred and thirty two"
+    901.in_full.must_equal "nine hundred and one"
+    509.in_full.must_equal "five hundred and nine"
   end
 
 end
