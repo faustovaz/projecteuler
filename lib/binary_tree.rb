d579167a0fcd13f2cf94b560a31507ddc2863ebc
faustovaz <faustovaz@gmail.com>
2016-04-11 18:02:38 -0300

    Implementing a binary tree

---
+++ lib/binary_tree.rb
@@ -0,0 +1,20 @@
+class Node
+  attr_reader :value, :left_node, :right_node
+  def initialize(value, root_node, left_node, right_node)
+    @value = value
+    @root_node = root_node
+    @left_node = left_node
+    @right_node = right_node
+  end
+end
+
+class BinaryTree
+  def initialize
+
+  end
+  def add_left_node(node)
+  end
+
+  def add_right_node(node)
+  end
+end
