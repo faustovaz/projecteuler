d6278114623f3f6e2c34c05542b20b5f5d82d209
Jefferson Fausto Vaz <faustovaz@gmail.com>
2016-04-14 23:54:28 -0300

    Solving the given example of the problem 18

--- 018.rb
+++ 018.rb
@@ -3,4 +3,10 @@ triangle ="""3
 2 4 6
 8 5 9 3"""
 
-p triangle.split("\n").map(&:split).map{ |elem| elem.map(&:to_i) }
+matrix = triangle.split("\n").map(&:split).map{ |elem| elem.map(&:to_i) }
+for i in (matrix.size - 2).downto 0 do
+  for j in 0..(matrix[i].size - 1) do
+    matrix[i][j] = [ matrix[i][j] + matrix[i+1][j], matrix[i][j] + matrix[i+1][j+1] ].max
+  end
+end
+p matrix[0][0]
