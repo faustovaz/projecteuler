c018fa6a6ed3d6ebaae883031d791d56dd46bd67
Jefferson Fausto Vaz <faustovaz@gmail.com>
2016-04-15 00:38:13 -0300

    Refactoring

--- 018.rb
+++ 018.rb
@@ -1,12 +1,19 @@
-triangle ="""3
+triangle =
+"""
+3
 7 4
 2 4 6
-8 5 9 3"""
+8 5 9 3
+"""
+matrix = triangle.split("\n")
+          .map(&:split)
+          .select{ |line| not line.empty? }
+          .map{ |array| array.map(&:to_i) }
 
-matrix = triangle.split("\n").map(&:split).map{ |elem| elem.map(&:to_i) }
 for i in (matrix.size - 2).downto 0 do
   for j in 0..(matrix[i].size - 1) do
-    matrix[i][j] = [ matrix[i][j] + matrix[i+1][j], matrix[i][j] + matrix[i+1][j+1] ].max
+    matrix[i][j] = [ matrix[i][j] + matrix[i+1][j],
+                      matrix[i][j] + matrix[i+1][j+1] ].max
   end
 end
 p matrix[0][0]
