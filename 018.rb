cdd3b579cee47b7ad11361b5f44810d8f4b962e5
faustovaz <faustovaz@gmail.com>
2016-04-15 11:00:21 -0300

    Creating a function an playing a little bit with exception in ruby

--- 018.rb
+++ 018.rb
@@ -1,19 +1,24 @@
-triangle =
+
+def max_path_sum(triangle)
+  for i in (triangle.size - 2).downto 0 do
+    for j in 0..(triangle[i].size - 1) do
+      triangle[i][j] = [ triangle[i][j] + triangle[i+1][j],
+                        triangle[i][j] + triangle[i+1][j+1] ].max
+    end
+  end
+  triangle.first.first
+end
+
+
+given_example =
 """
 3
 7 4
 2 4 6
 8 5 9 3
-"""
-matrix = triangle.split("\n")
-          .map(&:split)
-          .select{ |line| not line.empty? }
-          .map{ |array| array.map(&:to_i) }
+""".split("\n")
+    .map(&:split)
+    .select{ |line| not line.empty? }
+    .map{ |array| array.map(&:to_i) }
 
-for i in (matrix.size - 2).downto 0 do
-  for j in 0..(matrix[i].size - 1) do
-    matrix[i][j] = [ matrix[i][j] + matrix[i+1][j],
-                      matrix[i][j] + matrix[i+1][j+1] ].max
-  end
-end
-p matrix[0][0]
+raise "oops, this is wrong" unless max_path_sum(given_example) == 23
