40dd761523029160e9b4ea197793c84a9a05c595
faustovaz <faustovaz@gmail.com>
2016-04-20 16:29:27 -0300

    Solving problem 21

--- 021.rb
+++ 021.rb
@@ -6,6 +6,7 @@ divisors_sum = lambda{ |given_number|
   given_number.divisors.sort.reverse.drop(1).reduce(:+)
 }
 
-p (1..9999).select{ |number|
-  divisors_sum.call(divisors_sum.call number).eql? number
+p (1..10000).select{ |number|
+  not divisors_sum.call(number).eql? number and
+    divisors_sum.call(divisors_sum.call number).eql? number
 }.reduce(:+)
