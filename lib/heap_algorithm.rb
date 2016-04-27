e67410d45ad458441ce68fe782aa94b84ab1c518
faustovaz <faustovaz@gmail.com>
2016-04-27 18:18:24 -0300

    Testing heap's algorithm

--- lib/heap_algorithm.rb
+++ lib/heap_algorithm.rb
@@ -9,11 +9,14 @@ class Heaps
   end
 
   def permute(array, size)
-    @permutations << array if size.eql? 1
-    for i in (0..size - 1) do
+    puts "#{array}, #{size}" if size.eql? 1
+    for i in (0..(size - 1)) do
       permute(array, size - 1)
       array[0], array[size - 1] = array[size - 1], array[0] if size.odd?
       array[i], array[size - 1] = array[size - 1], array[i] if size.even?
     end
   end
 end
+
+h = Heaps.new
+h.permute([0,1,2], [0,1,2].size)
