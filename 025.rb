a719a82808741e4dfc1b73d4cf77a5ac43c422fb
faustovaz <faustovaz@gmail.com>
2016-05-02 17:26:52 -0300

    Using unless... please

--- 025.rb
+++ 025.rb
@@ -5,7 +5,7 @@ require File.expand_path 'lib/generators'
 include Generators
 
 #Given example:
-raise "wrooong" if not (fibbonaci_generator.take(
+raise "wrooong" unless (fibbonaci_generator.take(
       (fibbonaci_generator.take_while{ |fib| fib.to_s.size < 3 }.size + 1)
 ).size + 1).eql? 12
 
