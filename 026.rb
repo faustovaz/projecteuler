a6a6f6f032509c28c2f0ea95991d432c16d0413f
Jefferson Fausto Vaz <faustovaz@gmail.com>
2016-05-09 23:42:45 -0300

    Naming used lambda to modular_power

--- 026.rb
+++ 026.rb
@@ -2,14 +2,17 @@ require File.expand_path 'lib/generators'
 
 include Generators
 
-#https://en.wikipedia.org/wiki/Fermat's_little_theorem
-fermat_theorem = lambda{ |auxiliar, prime|
+
+#https://en.wikipedia.org/wiki/Modular_exponentiation
+modular_power = lambda{ |auxiliar, prime|
   ((10 ** auxiliar).modulo prime)
 }
 
 primes = prime_numbers_generator.take_while{ |prime| prime < 1000 }.reverse
+
+#https://en.wikipedia.org/wiki/Fermat's_little_theorem
 primes.each{ |prime|
   auxiliar = 1
-  while not fermat_theorem.call(auxiliar, prime).eql? 1 do auxiliar+=1 end
+  while not modular_power.call(auxiliar, prime).eql? 1 do auxiliar+=1 end
   break if (prime - 1).eql? auxiliar
 }
