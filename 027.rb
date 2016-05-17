fe7dda5aa8b4068cd364850febeab2dcfd9c12f8
Jefferson Fausto Vaz <jefferson.vaz@unila.edu.br>
2016-05-17 18:24:31 -0300

    Trying exercise 27

--- 027.rb
+++ 027.rb
@@ -8,18 +8,31 @@ quadratic_form = lambda{ |var, a, b|
     (var ** 2) + (a * var) + b
 }
 
-size_of_primes = lambda{ |var, a, b|
-  primes = prime_numbers_generator.take_while{ |prime|
-    prime < quadratic_form.call(var, a, b) + 1
-  }
-  (0..var).map{ |n| quadratic_form.call n, a, b }.select{ |n|
-    primes.include? n
-  }.size
+it_generates_prime = lambda{ |var, a, b|
+  value = quadratic_form.call(var, a, b)
+  primes = prime_numbers_generator.take_while{ |prime| prime < value + 1 }
+  primes.include? value
 }
 
+size_of_primes = lambda{ |a, b|
+  length = 0
+  while it_generates_prime.call(length, a, b) do length += 1 end
+  length
+}
 
 #first given example
-raise "This is wrooong" unless size_of_primes.call(39, 1, 41).eql? 40
+raise "This is wrooong" unless size_of_primes.call(1, 41).eql? 40
 
 #second given example
-raise "This is wrooong" unless size_of_primes.call(79, -79, 1601).eql? 80
+raise "This is wrooong" unless size_of_primes.call(-79, 1601).eql? 80
+
+largerst_size = 0
+largerst_product = 0
+(-999..1000).each{ |a|
+  (-999..1000).each{ |b|
+    length = size_of_primes.call(a,b)
+    largerst_size, largerst_product = length, (a * b) if length < largerst_size
+  }
+}
+p largerst_size
+p largerst_product
